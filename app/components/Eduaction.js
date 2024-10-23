8ac8bf272f8ecc08faba9e4ac351a828ef48cbb9
Hadikhan4535 <pakistanigamer4535@gmail.com>
2024-10-23 06:34:51 -0700

    complete project

--- app/components/Eduaction.js
+++ app/components/Eduaction.js
@@ -12,7 +12,7 @@ const Eduaction = () => {
       <div className="main flex justify-center">
         <div className="right-section flex flex-col gap-4 text-[#242b41] lg:w-full xl:gap-y-12 lg:flex-row lg:flex-wrap lg:gap-0 lg:gap-y-8 2xl:mx-24 2xl:w-[80%]">
           <div className="1 flex gap-1 items-center md:mx-14 lg:mx-0 lg:w-1/2">
-            <RiMedalLine className=" h-8 w-8 lg:h-12 lg:w-12" />
+            <RiMedalLine className=" h-8 w-8 lg:h-12 lg:w-12 max-sm:w-12" />
             <div className="flex flex-col">
               <div className="degree font-semibold text-lg md:text-xl lg:text-2xl xl:text-3xl">
                 Matriculation
@@ -23,7 +23,7 @@ const Eduaction = () => {
             </div>
           </div>
           <div className="2 flex gap-1 items-center md:mx-14 lg:mx-0 lg:w-1/2">
-            <RiMedalLine className=" h-8 w-8 lg:h-12 lg:w-12" />
+            <RiMedalLine className=" h-8 w-8 lg:h-12 lg:w-12 max-sm:w-12" />
             <div className="">
               <div className="degree font-semibold text-lg md:text-xl lg:text-2xl xl:text-3xl">
                 Intermediate
@@ -34,10 +34,10 @@ const Eduaction = () => {
             </div>
           </div>
           <div className="3 flex gap-1 items-center md:mx-14 lg:mx-0 lg:w-1/2">
-            <RiMedalLine className="h-8 w-8 lg:h-12 lg:w-12 " />
-            <div className="">
+            <RiMedalLine className="h-8 w-8 lg:h-12 lg:w-12 max-sm:w-10 max-sm:ml-2" />
+            <div className="max-sm:ml-2">
               <div className="degree font-semibold text-lg md:text-xl lg:text-2xl xl:text-3xl">
-                BS Computer Science
+              BS Computer Science
               </div>
               <div className="institute text-sm text-gray-500 md:text-base lg:text-lg">
                 Quaid-e-Awam University | 2022 to 2026 (Expected)
@@ -45,13 +45,13 @@ const Eduaction = () => {
             </div>
           </div>
           <div className="4 flex gap-1 items-center md:mx-14 lg:mx-0 lg:w-1/2">
-            <RiMedalLine className="h-8 w-8 lg:h-12 lg:w-12 " />
-            <div className="">
+            <RiMedalLine className="h-8 w-8 lg:h-12 lg:w-12 max-sm:w-10 max-sm:ml-2" />
+            <div className="max-sm:ml-2">
               <div className="degree font-semibold text-lg md:text-xl lg:text-2xl xl:text-3xl">
                 Diploma in IT
               </div>
               <div className="institute text-sm text-gray-500 md:text-base lg:text-lg">
-                Lal Computer Institute | SBTE Karachi | 2021 to 2022
+                Lal Computer Institute | SBTE Karachi | 2021 to 2022
               </div>
             </div>
           </div>
